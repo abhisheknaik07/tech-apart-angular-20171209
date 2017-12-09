39f1f2ac06f23840572568f8d7f4db61f03387e8
Abhishek Naik <Abhishek@odlindia.com>
2017-12-09 13:34:44 +0530

    Pushing Changes

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Fly with Angular';
+  tripName="Super Flights to Mars";
+  tripPrice="18200";
+  tripDescription="Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.";
+  tripDuration="2 Earth Years";
+  bookBtn="Book Trip";
 }
